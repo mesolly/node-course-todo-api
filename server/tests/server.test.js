8697df14ee4d344378e5c39d0130274a1b1f68d3
mesolly <saurav.kandari.123@gmail.com>
2021-04-07 15:21:12 +0530

    'TestDELETE:id'

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -101,3 +101,39 @@ describe('GET /todos/:id',()=>{
         .end(done) ;
     });
 });
+
+describe('DELETE /todos/:id',()=>{
+    /*it('Should delete todo',(done)=>{
+        var id = todos[1]._id.toHexString() ;
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.todo._id).toBe(id) ;
+        })
+        .end((err,res)=>{
+            if(err){
+                return done(err) ;
+            }
+            Todo.findById(id).then((todo)=>{
+                expect(todo).toNotExist();
+                done();
+            }).catch((err) => done(err));
+        });
+
+    });*/
+    it('Should return 404 if todo not found',(done)=>{
+        var id = new ObjectID().toHexString() ;
+
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+    it('Should return 400 if object id invalid',(done)=>{
+        request(app)
+        .delete('/todos/123')
+        .expect(400)
+        .end(done) ;
+    });
+});
