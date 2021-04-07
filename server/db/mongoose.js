b016bce8136833651772d7066fc7a167a6728e0a
mesolly <saurav.kandari.123@gmail.com>
2021-04-07 13:46:23 +0530

    'MongoDBConnect'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose') ;
 
+var url = 'mongodb+srv://solly:<Saurav7$>@cluster0.7bvqw.mongodb.net/TodoApp?retryWrites=true&w=majority';
 mongoose.Promise = global.Promise ;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(url || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose :mongoose
