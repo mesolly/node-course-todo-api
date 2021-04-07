86fe1a710ef358062fe8c4126a72303d2361b86b
mesolly <saurav.kandari.123@gmail.com>
2021-04-07 14:08:34 +0530

    'Heroku/variable'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose') ;
 
-var url = 'mongodb+srv://solly:<Saurav7$>@cluster0.7bvqw.mongodb.net/TodoApp?retryWrites=true&w=majority';
+
 mongoose.Promise = global.Promise ;
-mongoose.connect(url || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose :mongoose
