8a8039a24d45dcca0734832240b2bf68c83e54a0
mesolly <61472454+mesolly@users.noreply.github.com>
2021-05-22 11:06:48 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose') ;
 
 
 mongoose.Promise = global.Promise ;
-mongoose.connect('mongodb+srv://solly:Saurav7$@cluster0.7bvqw.mongodb.net/TodoApp?retryWrites=true&w=majority',{useNewUrlParser: true}, { useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://@cluster0.7bvqw.mongodb.net/TodoApp?retryWrites=true&w=majority',{useNewUrlParser: true}, { useUnifiedTopology: true });
 
 module.exports = {
     mongoose :mongoose
